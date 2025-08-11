9bfcc531a6310061342c366be5562e02998042dc
Doniel Acosta <donielacosta1995@gmail.com>
2025-08-10 22:22:08 -0400

    diferencia entre clases y funcion

--- src/ClassState.js
+++ src/ClassState.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react'
 
 export default class ClassState extends Component {
+    constructor(){
+        super();
+        this.state ={
+            error:false,
+        };
+    }
   render() {
     return (
       <div>
-        <h3>Eliminar ClassState</h3>
+        <h3>Eliminar {this.props.name}</h3>
         <p>Por favor, escriba el código de seguridad.</p>
+      {this.state.error &&(<p>Error: el codigo es incorrecto</p>)}
+
         <input type='text' placeholder='código de seguridad'/>
-        <button>Comprobar</button>
+        <button
+            onClick={()=>this.setState({error: !this.state.error})}
+        >
+            Comprobar</button>
       </div>
     )
   }
--- src/UseState.js
+++ src/UseState.js
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function UseState() {
+export default function UseState({name}) {
+
+    const [error,setError] = useState(true);
   return (
     <div>
-      <h3>Eliminar UseState</h3>
+      <h3>Eliminar {name}</h3>
       <p>Por favor, escriba el código de seguridad.</p>
+      {error &&(<p>Error: el codigo es incorrecto</p>)}
       <input type='text' placeholder='código de seguridad'/>
-      <button>Comprobar</button>
+      <button
+        onClick={() => setError(!error)}
+      >Comprobar</button>
     </div>
   )
 }

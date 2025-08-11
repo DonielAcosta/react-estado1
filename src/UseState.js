a5af292ee2f4cda715eccd5466132de82d62b0ba
Doniel Acosta <donielacosta1995@gmail.com>
2025-08-11 11:10:17 -0400

    estado independiente

--- src/UseState.js
+++ src/UseState.js
@@ -1,17 +1,28 @@
 import React, { useState,useEffect } from 'react'
 
+const SECURITY_CODE ='paradigma';
+
 export default function UseState({name}) {
 
-    const [error,setError] = useState(true);
+    const [error,setError] = useState(false);
     const [loading,setloading] = useState(false);
+    const [value,setValue] = useState('');
+
+    console.log(value);
     useEffect(() => {
         console.log("empezamos el efecto");
         if(!!loading){
 
             setTimeout(() =>{
-            console.log("Iniciando Validacion");
-            setloading(false)
-            console.log("Terminamos Validacion");
+              console.log("Iniciando Validacion");
+              if(value=== SECURITY_CODE){
+                setloading(false)
+              }else{
+                setError(true);
+                setloading(false)
+
+              }
+              console.log("Terminamos Validacion");
             },3000)
         }
         console.log("Terminamos el efecto");
@@ -21,11 +32,26 @@ export default function UseState({name}) {
     <div>
       <h3>Eliminar {name}</h3>
       <p>Por favor, escriba el código de seguridad.</p>
-      {error &&(<p>Error: el codigo es incorrecto</p>)}
+      
+      {(error && !loading) &&(<p>Error: el codigo es incorrecto</p>)}
+
+
       {loading && (<p>Cargando...</p>)}
-      <input type='text' placeholder='código de seguridad'/>
+
+      <input 
+        type='text'
+        placeholder='código de seguridad'
+        value={value}
+        onChange={ (event) =>{
+          // setError(false);
+          setValue(event.target.value);
+        }}
+      />
       <button
-        onClick={() => setloading(true)}
+        onClick={() => {
+          // setError(false)  //aqui va 
+          setloading(true)
+        }}
       >Comprobar</button>
     </div>
   )
